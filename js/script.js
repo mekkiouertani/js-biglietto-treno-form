8257c10e7a641d701b5fc5ba134b472d3127c8e4
mekkiouertani <mekki.ouertani@gmail.com>
2023-10-10 15:30:53 +0200

    added parseInt to prompt

--- js/script.js
+++ js/script.js
@@ -2,7 +2,10 @@ const KmPrice = 0.21;
 const discountUnder18 = 20/100; //20%
 const discountOver65 = 40/100;  //40%
 let km, age, price;
-km = prompt('Quanti km vuole percorrere?');
-age = prompt('Qual è la sua età?');
+km = parseInt(prompt('Quanti km vuole percorrere?'));
+age = parseInt(prompt('Qual è la sua età?'));
 price = KmPrice * km ;
 console.log('km',km, "/",'età',age);
+if (isNaN(km) || isNaN(age)) {
+    location.reload();
+}
