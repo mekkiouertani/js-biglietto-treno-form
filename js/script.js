427f68748e1e9df008b82ca2e8cd79d633efa8b8
mekkiouertani <mekki.ouertani@gmail.com>
2023-10-10 15:39:28 +0200

    added conditions to js

--- js/script.js
+++ js/script.js
@@ -4,8 +4,15 @@ const discountOver65 = 40/100;  //40%
 let km, age, price;
 km = parseInt(prompt('Quanti km vuole percorrere?'));
 age = parseInt(prompt('Qual è la sua età?'));
-price = KmPrice * km ;
+price = KmPrice * km ; 
 console.log('km',km, "/",'età',age);
-if (isNaN(km) || isNaN(age)) {
+if (isNaN(km) || isNaN(age)) { 
     location.reload();
 }
+
+if (age < 18) {
+    price -= discountUnder18 * price;
+} else if (age > 65){
+    price -= discountOver65 * price;
+}
+console.log('prezzo del biglietto €',price);
