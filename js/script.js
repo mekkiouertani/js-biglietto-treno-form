5194149527f6a870e3b762053dbff5a3ed2f9910
mekkiouertani <mekki.ouertani@gmail.com>
2023-10-10 18:11:34 +0200

    fix condition isNaN, updated console.log

--- js/script.js
+++ js/script.js
@@ -1,30 +1,28 @@
-//CONST
+//VAR
 const kmPrice = 0.21;
 const discountUnder18 = 20/100; //20%
 const discountOver65 = 40/100;  //40%
 const button = document.querySelector('.btn');
-
-//LET
-let km, age, price;
-price = kmPrice * km ;
-
+let km, age, price; //dichiaro le variabili
 //EVENT LISTERER
-button.addEventListener('click', function(){
-    km = parseInt(document.getElementById('kmToTravel').value);
-    age = parseInt(document.getElementById('passengerAge').value);
-    console.log('km',km, "/",'età',age);    
-    //IF solo caratterri numerici
-    if (isNaN(kmToTravel) || isNaN(passengerAge)) { 
-        location.reload();
-    } 
-    //IF sconto età
-    if (age < 18) {
-        price -= discountUnder18 * price;
-    } else if (age > 65){
-        price -= discountOver65 * price;
-    } console.log('prezzo del biglietto €',price.toFixed(2)); 
-});
-
+button.addEventListener('click', 
+    function(){
+        km = parseInt(document.getElementById('kmToTravel').value);
+        age = parseInt(document.getElementById('passengerAge').value);
+        price = kmPrice * km ;
+        console.log('km',km, "/",'età',age);  
+        //IF solo caratterri numerici
+        if (isNaN(km) || isNaN(age)) { 
+            location.reload();
+        } 
+        //IF sconto età
+        if (age < 18) {
+            price -= discountUnder18 * price;
+        } else if (age > 65){
+            price -= discountOver65 * price;
+        }   console.log('prezzo del biglietto €',price.toFixed(2));  
+    }
+);
 
 //CONSOLE LOG
 console.log(button);
