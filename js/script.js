1c9e3dd6ee16afc2855d471a3e72340abc95f87c
mekkiouertani <mekki.ouertani@gmail.com>
2023-10-10 15:26:08 +0200

    added variables to js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 const KmPrice = 0.21;
 const discountUnder18 = 20/100; //20%
 const discountOver65 = 40/100;  //40%
+let km, age, price;
+km = prompt('Quanti km vuole percorrere?');
+age = prompt('Qual è la sua età?');
+price = KmPrice * km ;
+console.log('km',km, "/",'età',age);
